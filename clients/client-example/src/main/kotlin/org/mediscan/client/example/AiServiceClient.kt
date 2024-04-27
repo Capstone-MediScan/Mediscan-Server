c3679b04bb3ee93d47370610243a078e608ba5a0
hojinida <opqrs1103@gmail.com>
2024-04-27 23:32:48 +0900

    [Feat]: AiServiceClient 구현

---
+++ clients/client-example/src/main/kotlin/org/mediscan/client/example/AiServiceClient.kt
@@ -0,0 +1,13 @@
+package org.mediscan.client.example
+
+import org.mediscan.client.example.model.PillIdentificationRequestDto
+import org.mediscan.client.example.model.PillIdentificationResponseDto
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+
+@FeignClient(name = "ai-service", url = "\${ai.service.url}")
+interface AiServiceClient {
+    @PostMapping("/identify-pill")
+    fun identifyPill(@RequestBody requestDto: PillIdentificationRequestDto): List<PillIdentificationResponseDto>
+}
