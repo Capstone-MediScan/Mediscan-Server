7d6bc9a23dab606f786358f16e1f91bf04e05f06
Park Sejin <jinpark99@naver.com>
2024-05-27 16:17:05 +0900

    chore: ktlint 적용

--- core/core-api/src/main/kotlin/org/mediscan/core/api/domain/PillService.kt
+++ core/core-api/src/main/kotlin/org/mediscan/core/api/domain/PillService.kt
@@ -6,15 +6,27 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
-class PillService (
+class PillService(
     private val pillManager: PillManager,
-    private val pillReader: PillReader
-){
-    fun identifyPill(frontImage: MultipartFile, backImage: MultipartFile,
-                     fillShape: String, frontMarking: String, backMarking: String ): List<PillDomainIdentificationResponseDto> {
-
+    private val pillReader: PillReader,
+) {
+    fun identifyPill(
+        frontImage: MultipartFile,
+        backImage: MultipartFile,
+        fillShape: String,
+        frontMarking: String,
+        backMarking: String,
+    ): List<PillDomainIdentificationResponseDto> {
         val pillDomainResponse = pillManager
-            .identifyPill(PillDomainIdentificationRequestDto(frontImage, backImage, fillShape, frontMarking, backMarking))
+            .identifyPill(
+                PillDomainIdentificationRequestDto(
+                    frontImage,
+                    backImage,
+                    fillShape,
+                    frontMarking,
+                    backMarking,
+                ),
+            )
 
         return pillDomainResponse
     }
